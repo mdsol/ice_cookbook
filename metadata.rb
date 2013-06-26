afd7e414e8624dbb79b6bf5f4d96b684a246adec
Ray Rodriguez <rarodriguez@mdsol.com>
2013-06-26 03:14:00 -0400

    Removed license type until we can figure out if this will be interpreted under apache or mit license.

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,6 @@
 name             'ice'
 maintainer       'Ray Rodriguez'
 maintainer_email 'rayrod2030@gmail.com'
-license          ''
 description      'Installs/Configures ice'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.7'
