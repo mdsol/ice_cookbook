0ec0abc0278f3d9fc5bf78e20cede98f02f65d13
Ray Rodriguez <rarodriguez@mdsol.com>
2013-06-27 17:48:12 -0400

    Updated maintainer to my email.

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'ice'
 maintainer       'Medidata Solutions'
-maintainer_email 'cookbooks@mdsol.com'
+maintainer_email 'rarodriguez@mdsol.com'
 license          "Apache 2.0"
 description      'Installs/Configures ice'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
