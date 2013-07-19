023b4b720557ab684183e0d909bbe351c218f16c
Ray Rodriguez <rarodriguez@mdsol.com>
2013-07-19 15:45:29 -0400

    Added ice.role property to attributes with default of 'ice' and included it in java options in recipe.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'rarodriguez@mdsol.com'
 license          "Apache 2.0"
 description      'Installs/Configures ice'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.9'
+version          '0.2.10'
 
 %w{ ubuntu centos }.each do |os|
     supports os
