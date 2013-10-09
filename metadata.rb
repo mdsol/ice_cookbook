a7a606d7f8f266923c0bc928ef265c2f31ae04a3
Ankit Shah <ankit@yhmg.com>
2013-10-09 17:19:27 -0400

    Updated version to 0.2.13.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'rarodriguez@mdsol.com'
 license          "Apache 2.0"
 description      'Installs/Configures ice'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.12'
+version          '0.2.13'
 
 %w{ ubuntu centos }.each do |os|
     supports os
