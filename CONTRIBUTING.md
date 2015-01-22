b96500d744ce44d40e7fd61f4da5c7f5c6b99125
Ray Rodriguez <rayrod2030@gmail.com>
2015-01-22 11:46:17 -0500

    Updated README and all related documentation to community standards

---
+++ CONTRIBUTING.md
@@ -0,0 +1,100 @@
+Contributing to the Ice Cookbook
+====================================
+The Ice cookbook uses GitHub to triage, manage, and track issues and pull
+requests to the cookbook. GitHub has excellent documentation on how to
+[fork a repository and start contributing](https://help.github.com/articles/fork-a-repo.).
+
+All contributors are welcome to submit patches, but we ask you keep the
+following guidelines in mind:
+
+- [Coding Standards](#coding-standards)
+- [Testing](#testing)
+- [Prerequisites](#prerequisites)
+
+Please also keep in mind:
+
+- Be patient as not all items will be tested or reviewed immediately by the core
+team.
+- Be receptive and responsive to feedback about your additions or changes. The
+core team and/or other community members may make suggestions or ask questions
+about your change. This is part of the review process, and helps everyone to
+understand what is happening, why it is happening, and potentially optimizes
+your code.
+- Be understanding
+
+If you're looking to contribute but aren't sure where to start, check out the
+open issues.
+
+
+Will Not Merge
+--------------
+This second details Pull Requests that we will **not** merge.
+
+1. New features without accompanying Test Kitchen tests
+1. New features without accompanying usage documentation
+
+
+Coding Standards
+----------------
+The submitted code should be compatible with the standard Ruby coding guidelines.
+Here are some additional resources:
+
+- [Ruby Style Guide](https://github.com/bbatsov/ruby-style-guide)
+- [GitHub Styleguide](https://github.com/styleguide/ruby)
+
+This cookbook is equipped with Rubocop, which will fail the build for violating
+these standards.
+
+
+Testing
+-------
+Whether your pull request is a bug fix or introduces new classes or methods to the
+project, we kindly ask that you include tests for your changes. Even if it's just a
+small improvement, a test is necessary to ensure the bug is never re-introduced.
+
+We understand that not all users are familiar with the testing ecosystem. This cookbook
+is fully-tested using [Foodcritic](https://github.com/acrmp/foodcritic),
+[Rubocop](https://github.com/bbatsov/rubocop), and
+[Test Kitchen](https://github.com/test-kitchen/test-kitchen) with
+[Serverspec](https://github.com/serverspec/serverspec) bussers.
+
+
+Prerequisites
+-------------
+Developing this cookbook requires a sane Ruby 1.9+ environment with `bundler` installed.
+In order to run the Test Kitchen integration suite, you must also have Vagrant and
+VirtualBox installed:
+
+- [Vagrant](https://vagrantup.com)
+- [VirtualBox](https://virtualbox.org)
+
+
+Process
+-------
+1. Clone the git repository from GitHub:
+
+        $ git clone git@github.com:mdsol/ice_cookbook.git
+
+2. Install the dependencies using bundler:
+
+        $ bundle install
+
+3. Create a branch for your changes:
+
+        $ git checkout -b my_bug_fix
+
+4. Make any changes
+5. Write tests to support those changes.
+6. Run the tests:
+
+        $ bundle exec rake
+
+7. Assuming the tests pass, open a Pull Request on GitHub
+
+
+Do's and Don't's
+----------------
+- **Do** include tests for your contribution
+- **Do NOT** break existing behavior (unless intentional)
+- **Do NOT** modify the version number in the `metadata.rb`
+- **Do NOT** modify the CHANGELOG
--- attributes/default.rb
+++ attributes/default.rb
@@ -1,40 +1,74 @@
-# Ice version
+# Ice version to download and install.  These versions are packaged and hosted
+# by Medidata Solutions until we can get the Netflix Ice team to package and
+# host official ice releases.  If you wish to install the latest stable version
+# from [Netflix](https://github.com/netflix/ice#download-snapshot-builds)
+# directly, provide `stable`. Note: this option will always install the latest
+# version even if its not backwards compatible.
 node.default['ice']['version']                              = '0.0.4'
+
+# Checksum for Ice WAR file.
 node.default['ice']['checksum']                             = 'eb9e7503585553bdebf9d93016bcbe7dc033c21e2b1b2f0df0978ca2968df047'
+
+# HTTP URL for Ice WAR file.
 node.default['ice']['war_url']                              = 'https://s3.amazonaws.com/dl.imedidata.net/ice'
+
+# Will force a redeploy of the Ice WAR file.
 node.default['ice']['force_redeploy']                       = false
 
+# Organization name that is displayed in the UI header.
 node.default['ice']['company_name']                         = nil
 
+# Enables the ice processor.
 node.default['ice']['processor']['enabled']                 = true
-node.default['ice']['processor']['local_dir']                = '/var/ice_processor'
+
+# Local work directory for the Ice processor.
+node.default['ice']['processor']['local_dir']               = '/var/ice_processor'
+
+# Optional. Work around https://github.com/Netflix/ice/issues/100 by pre-creating
+# the directories it expects to find. Default: false
 node.default['ice']['processor']['issue_100_workaround']    = false
 
-# S3 keys for accessing billing files
+# AWS access key id used for accessing AWS detailed billing files from S3.
 node.default['ice']['billing_aws_access_key_id']            = nil
+# AWS secret key used for accessing AWS detailed billing files from S3.
 node.default['ice']['billing_aws_secret_key']               = nil
 
+# Name of the S3 bucket containing the AWS detailed billing files.
 node.default['ice']['billing_s3_bucket_name']               = nil
+# Directory in the S3 billing bucket containing AWS detailed billing files.
 node.default['ice']['billing_s3_bucket_prefix']             = nil
 
-node.default['ice']['billing_payerAccountId']				= nil
-node.default['ice']['billing_accessRoleName']				= nil
-node.default['ice']['billing_accessExternalId']				= nil
+node.default['ice']['billing_payerAccountId']               = nil
+node.default['ice']['billing_accessRoleName']               = nil
+node.default['ice']['billing_accessExternalId']             = nil
 
+# Name of the S3 bucket that Ice uses for processed AWS detailed billing files.
+# This bucket is shared between the Ice processor and reader.
 node.default['ice']['work_s3_bucket_name']                  = nil
+
+# Directory in the S3 work bucket containing the processed AWS detailed billing files.
 node.default['ice']['work_s3_bucket_prefix']                = nil
 
+# Enables the Ice reader and installs the Nginx reverse proxy.
 node.default['ice']['reader']['enabled']                    = true
-node.default['ice']['reader']['local_dir']                   = '/var/ice_reader'
+# Local work directory for the Ice reader.
+node.default['ice']['reader']['local_dir']                  = '/var/ice_reader'
 
-node.default['ice']['start_millis']                          = 0
+# Specify the start time in milliseconds for the processor to start processing billing files.
+node.default['ice']['start_millis']                         = 0
 
+# Optional.  Hash mapping of AWS account names to account numbers.  This is used
+# within Ice to give accounts human readable names in the UI.
 node.default['ice']['accounts']                             = {}
 
+# Hash mapping accounts to IAM roles in order to allow reservations in that
+# account to be polled.
 node.default['ice']['reservation_capacity_poller']          = false
 
 node.default['ice']['iam_role']                             = 'ice'
 
+# Array of custom resource tags to have ice process.  As described in the ice
+# README you must explicitly enable these custom tags in your billing statements.
 node.default['ice']['custom_resource_tags']                 = []
 
 # # reservation period, possible values are oneyear, threeyear
@@ -42,29 +76,71 @@ node.default['ice']['reservation_period']                   = 'threeyear'
 # # reservation utilization, possible values are LIGHT, HEAVY
 node.default['ice']['reservation_utilization']               = 'HEAVY'
 
+# Currency sign to use in place of $
 node.default['ice']['currencySign']                          = nil
+
+# Conversion rate of USD to the above currency. For example, if 1 pound = 1.5
+# dollar, then the rate is 0.6666667.
 node.default['ice']['currencyRate']                          = nil
+
+# URL to highstock.js if you need to serve this over HTTPS (which the default
+# Highstock CDN does not currently support)
 node.default['ice']['highstockUrl']                          = nil
+
+#Monthly cache size.
 node.default['ice']['monthlycachesize']                      = nil
+
+# Cost per monitor metric per hour.
 node.default['ice']['cost_per_monitormetric_per_hour']       = nil
+
+# URL of Ice installation, used to create links in alert emails.
 node.default['ice']['urlPrefix']                             = nil
+
+# Email address from which Ice email alerts are sent (must be registered in SES)
 node.default['ice']['fromEmail']                             = nil
+
+# EC2 On-Demand hourly cost threshold at which an alert email should be sent.
 node.default['ice']['ondemandCostAlertThreshold']            = nil
+
+# Comma-separated list of recipients for the On-Demand cost alert emails.
 node.default['ice']['ondemandCostAlertEmails']               = nil
+
+# If set to `original`, Ice will use the original costs from the billing file
+# for Resource Groups.
 node.default['ice']['resourceGroupCost']                     = nil
+
+# Set to `true` to enable weekly cost emails.
 node.default['ice']['weeklyCostEmails']                      = nil
+
+# Email address from which weekly cost emails are sent (must be registered in SES)
 node.default['ice']['weeklyCostEmails_fromEmail']            = nil
+
+# Email address to which weekly cost emails will be BCCed.
 node.default['ice']['weeklyCostEmails_bccEmail']             = nil
+
+# How many weeks to include in the weekly cost emails
 node.default['ice']['weeklyCostEmails_numWeeks']             = nil
 
-# This hostname is used for the nginx reverse proxy configuration
+# Optional. Fully qualified domain name used for configuring the Nginx reverse
+# proxy on Ice readers/UI nodes.
 node.default['ice']['public_hostname']                      = nil
 
+# Nginx port configuration.
 node.default['ice']['nginx_port']                           = 80
+
+# Nginx site configuration chef template name.
 node.default['ice']['nginx_config']                         = 'nginx_ice_site.erb'
+
+# Nginx custom configuration cookbook.  Use this if you'd like to bypass the
+# default ice cookbook nginx configuration and implement your own templates
+# and recipes to configure Nginx for ice.
 node.default['ice']['nginx_config_cookbook']                = 'ice'
+
+# Whether nginx should route all requests to Tomcat, regardless of Host: header.  Default: false.
 node.default['ice']['nginx_default_server']                 = false
 
-# Logrotate options for catalina.out
+# How often to rotate catalina.out.
 node.default['ice']['logrotate_frequency']                  = 'weekly'
+
+# How many rotated copies of catalina.out to keep.
 node.default['ice']['logrotate_rotate']                     = 52
